588a7eea01bce48f3784c41d7478c5612dc6030b
nzegarra <nzegarraarteaga@gmail.com>
2018-03-13 15:05:52 -0500

    prueba de cliente servicio web con jersey, gerónimo

--- inteligo-sab/modules/backend-app-api/javabrian-sab-service/src/main/java/org/inteligo/sab/javabrians/service/JavaBriansService.java
+++ inteligo-sab/modules/backend-app-api/javabrian-sab-service/src/main/java/org/inteligo/sab/javabrians/service/JavaBriansService.java
@@ -3,11 +3,14 @@ package org.inteligo.sab.javabrians.service;
 
 import java.io.IOException;
 
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import javax.xml.ws.http.HTTPException;
 
-import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.HttpStatus;
-import org.apache.commons.httpclient.methods.GetMethod;
 import org.inteligo.sab.api.rest.javabrians.api.MessageApi;
 
 import org.osgi.service.component.annotations.Component;
@@ -29,22 +32,43 @@ public class JavaBriansService implements MessageApi {
 	
 	private static final Log LOGGER = LogFactoryUtil.getLog(JavaBriansService.class);
 	
+//	@Override
+//	public void getProfiles() {
+//		
+//		HttpClient client = new HttpClient();
+//		GetMethod method = new GetMethod("http://localhost:8085/JAXRSMessage/rest/profiles");
+//		method.setRequestHeader("Accept", "application/json");
+//		LOGGER.info("INIT CAll the service of JAva Brians API JAX RS with HTTPCLIENT");
+//		try {
+//			int statusCode = client.executeMethod(method);
+//			LOGGER.info("STATUS CODE: "+statusCode);
+//			if(statusCode == HttpStatus.SC_OK){
+//				LOGGER.info("SUCCES Method API for JavaBrians....");
+//				byte []responseBody = method.getResponseBody();
+//				LOGGER.info("RESPONSE: "+new String(responseBody));
+//			}
+//		} catch (IOException | HTTPException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}finally {
+//			LOGGER.info("FINALLY RESOURCE API");
+//		}
+//	}
+	
 	@Override
 	public void getProfiles() {
 		
-		HttpClient client = new HttpClient();
-		GetMethod method = new GetMethod("http://localhost:8085/JAXRSMessage/rest/profiles");
-		method.setRequestHeader("Accept", "application/json");
+//		HttpClient client = new HttpClient();
+//		GetMethod method = new GetMethod("http://localhost:8085/JAXRSMessage/rest/profiles");
+//		method.setRequestHeader("Accept", "application/json");
 		LOGGER.info("INIT CAll the service of JAva Brians API JAX RS with HTTPCLIENT");
 		try {
-			int statusCode = client.executeMethod(method);
-			LOGGER.info("STATUS CODE: "+statusCode);
-			if(statusCode == HttpStatus.SC_OK){
-				LOGGER.info("SUCCES Method API for JavaBrians....");
-				byte []responseBody = method.getResponseBody();
-				LOGGER.info("RESPONSE: "+new String(responseBody));
-			}
-		} catch (IOException | HTTPException e) {
+			Client client = ClientBuilder.newBuilder().newClient();
+			WebTarget target = client.target("http://localhost:8085/JAXRSMessage/rest/profiles");
+			Invocation.Builder builder = target.request(MediaType.APPLICATION_JSON_TYPE);
+			Response response = builder.get();
+			LOGGER.info(response.readEntity(String.class));
+			}catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}finally {
